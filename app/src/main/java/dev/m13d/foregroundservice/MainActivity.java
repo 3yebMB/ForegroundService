54c5f4e67e20c855535c5244df187a1b605da2a3
Michael <3yebmb@gmail.com>
2019-12-21 15:32:13 +0500

    initial example project

---
+++ app/src/main/java/dev/m13d/foregroundservice/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.m13d.foregroundservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
