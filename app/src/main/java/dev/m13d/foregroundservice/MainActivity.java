ea8783a497bdc9e904535d481dc026725d548045
Michael <3yebmb@gmail.com>
2019-12-24 09:38:08 +0500

    add activity and main class + string resource

--- app/src/main/java/dev/m13d/foregroundservice/MainActivity.java
+++ app/src/main/java/dev/m13d/foregroundservice/MainActivity.java
@@ -1,14 +1,37 @@
 package dev.m13d.foregroundservice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextInput = findViewById(R.id.edit_text_input);
+    }
+
+    public void startService(View v) {
+        String input = editTextInput.getText().toString();
+
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        serviceIntent.putExtra("inputExtra", input);
+//        startService(serviceIntent);
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
     }
 }
