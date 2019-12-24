fd62367697f47d19d467aa2f34bb5a5794aad172
Michael <3yebmb@gmail.com>
2019-12-24 09:35:45 +0500

    add ExampleService (service) class

---
+++ app/src/main/java/dev/m13d/foregroundservice/ExampleService.java
@@ -0,0 +1,54 @@
+package dev.m13d.foregroundservice;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import static dev.m13d.foregroundservice.App.CHANNEL_ID;
+
+public class ExampleService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String input = intent.getStringExtra("inputExtra");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setContentTitle(getString(R.string.title_service));
+        builder.setContentText(input);
+        builder.setSmallIcon(R.drawable.ic_android_black_24dp);
+        builder.setContentIntent(pendingIntent);
+        Notification notification = builder
+                .build();
+
+        startForeground(1, notification);
+
+//        stopSelf();
+
+        return START_NOT_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
